965659c2285542fff4caad31cef149dbba0c0aea
Ritesh Dhurve <riteshdhurve2440@gmail.com>
2025-06-18 02:00:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let inp = document.querySelector("#city");
 let btn = document.querySelector("button");
-let api_key = "bfcba8417acd4e86baa110146251306"; // ✅ Correct key
+let api_key = "CREATE YOUR OWN KEY"; // ✅ Correct key
 
 btn.addEventListener("click", async () => {
     let city = inp.value.trim();
